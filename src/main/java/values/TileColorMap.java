e854808d71537cf1b6b45f28ed252a6bca70b0d5
Sai Kiran <sm4536@drexel.edu>
2019-11-26 20:59:28 -0500

    Inlet and Outlet Portals Added

--- src/main/java/values/TileColorMap.java
+++ src/main/java/values/TileColorMap.java
@@ -15,6 +15,8 @@ public final class TileColorMap {
 		tileColors.put(TileType.PASSABLE, Color.WHITE);
 		tileColors.put(TileType.NOT_PASSABLE, Color.BLACK);
 		tileColors.put(TileType.PLAYER, Color.GREEN);
+		tileColors.put(TileType.INLET, Color.GRAY);
+		tileColors.put(TileType.OUTLET, Color.RED);
 	}
 
 	public static Color get(TileType key) {
--- src/test/java/tiles/TileTypeTest.java
+++ src/test/java/tiles/TileTypeTest.java
@@ -10,6 +10,10 @@ public class TileTypeTest {
 
 	private static final char INVALID_CHAR = 'Z';
 	private static final char VALID_CHAR = ' ';
+	private static final char INVALID_INLET_CHAR = 'C';
+	private static final char VALID_INLET_CHAR = 'I';
+	private static final char INVALID_OUTLET_CHAR = 'D';
+	private static final char VALID_OUTLET_CHAR = 'O';
 
 	@Test
 	public void value_of() {
@@ -28,6 +32,36 @@ public class TileTypeTest {
 			TileType.getTileTypeByChar(INVALID_CHAR);
 		} catch (IllegalArgumentException exception) {
 			assertEquals(exception.getMessage(), TileType.INVALID_CHARACTER_PROVIDED_MESSAGE + "Z");
+		}
+	}
+
+	@Test
+	public void get_tile_type_by_char_valid_Inlet_char() {
+		TileType actual = TileType.getTileTypeByChar(VALID_INLET_CHAR);
+		assertEquals(TileType.INLET, actual);
+	}
+
+	@Test
+	public void get_tile_type_by_char_invalid_Inlet_char() {
+		try {
+			TileType.getTileTypeByChar(INVALID_INLET_CHAR);
+		} catch (IllegalArgumentException exception) {
+			assertEquals(exception.getMessage(), TileType.INVALID_CHARACTER_PROVIDED_MESSAGE + "C");
+		}
+	}
+
+	@Test
+	public void get_tile_type_by_char_valid_outlet_char() {
+		TileType actual = TileType.getTileTypeByChar(VALID_OUTLET_CHAR);
+		assertEquals(TileType.OUTLET, actual);
+	}
+
+	@Test
+	public void get_tile_type_by_char_invalid_outlet_char() {
+		try {
+			TileType.getTileTypeByChar(INVALID_OUTLET_CHAR);
+		} catch (IllegalArgumentException exception) {
+			assertEquals(exception.getMessage(), TileType.INVALID_CHARACTER_PROVIDED_MESSAGE + "D");
 		}
 	}
 }
